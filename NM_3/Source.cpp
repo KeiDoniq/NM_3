3f5cc1d384a63f89cdec812f32989216700b51e6
kioratulate <angelinapevtsova312@gmail.com>
2023-12-14 08:31:09 +0300

    добавила тесты

--- NM_3/Source.cpp
+++ NM_3/Source.cpp
@@ -7,20 +7,32 @@ int main()
 	srand(GetTickCount64());
 	int K = 1000;
 	std::ofstream out2("out2.txt");
-	for (int i = 10; i <= 50; i += 20)
+	for (int N = 10; N <= 50; N += 20)
 	{
-		for (int j = 2; j <= 50; j += 48)
-			for (double t = 1e-5; t >= 1e-8; t /= 1e3)
+		for (int lambda_range = 2; lambda_range <= 50; lambda_range += 48)
+			for (double eps = 1e-5; eps >= 1e-8; eps /= 1e3)
 			{
-				Matrix M(i, K, j, t);
-				M.Solve();
-
-				out2 << "\nr = " << M.get_r()
-					<< "\ndiff lambda = " << M.get_l_accur() << ",  lambda = " << std::setprecision(10) << M.get_l()
-					<< "\ndiff x= " << M.get_x_accur() << "\n" << M.ToString_X() 
-					<< "\nk = " << M.get_k() << "\n\n\n" << std::endl;
-
-
+				double av_acc_x(0), av_acc_l(0), av_r(0), av_k(0); int n_tests(50);
+				for (int test = 0; test < n_tests; ++test)
+				{
+					Matrix M(N, K, lambda_range, eps);
+					M.Solve();
+					/*if (M.get_k() != 1001)
+					{*/
+						av_acc_x += M.get_x_accur();
+						av_acc_l += M.get_l_accur();
+						av_r += M.get_r();
+						av_k += M.get_k();
+					/*}
+					else
+						test -= 1;*/
+				}
+				av_acc_x /= n_tests;
+				av_acc_l /= n_tests;
+				av_r /= n_tests;
+				av_k/= n_tests*1.0;
+				out2 << "N = "<<N<<" lambda range = "<<lambda_range<<" eps = "<<eps<<"\nav_diff_lambda = " << av_acc_l << " av_diff_x = " << av_acc_x
+				<< " av_r = " << av_r <<" av_k = " <<av_k << std::endl;
 			}
 	}
 }
